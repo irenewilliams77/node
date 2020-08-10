1207b6366e50953ef42589381572b532e2f42643
Irene Williams <europeandistribution@ymail.com>
2020-08-10 11:31:29 -0400

    added express

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var mysql      = require('mysql1');
+var express = require ('express')
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -12,7 +13,11 @@ connection.query('SELECT * from users', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results);
 });
-connection.query("insert into ")
+
+connection.query("insert into user(name) values('Irene Williams) "), function(error, results, fields){
+  if (error) trow error;
+  console.log('inserted')
+}
  
 connection.end();
 
